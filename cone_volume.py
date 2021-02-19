e4da3fbdeb53a62061f91cf5ba9988d24d72a744
Reilly <reilly.ford@gmail.com>
2021-02-19 16:34:54 -0500

    changed cone volume a little so it would display floats with decimals and integers without

--- cone_volume.py
+++ cone_volume.py
@@ -4,12 +4,12 @@ WRITE YOUR FUNCTION DEFINITION BELOW
 import math
 
 def print_cone_volume():
-    radius = int(input("What is the radius of the cone? Only positive numbers allowed.\n"))
-    if radius > 0:
-        height = int(input("What is the height of the cone? Only positive numbers allowed.\n"))
-        if height > 0:
-            volume = float(math.pi * (math.pow(radius, 2)) * (height / 3))
-            print("The volume of a cone with a radius of " + str(radius) + " and a height of " + str(height) + " is " + str(volume) + ".")
+    radius = str(input("What is the radius of the cone? Only positive numbers allowed.\n"))
+    if float(radius) > 0:
+        height = str(input("What is the height of the cone? Only positive numbers allowed.\n"))
+        if float(height) > 0:
+            volume = float(math.pi * (math.pow(float(radius), 2)) * (float(height) / 3))
+            print("The volume of a cone with a radius of " + radius + " and a height of " + height + " is " + str(volume) + ".")
         else:
             print("The value of the radius and the height must be positive.")
     else:
